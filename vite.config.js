a94d027be07a8cb08e980a404c3c53bcad330591
Codingpastor <dawicks@gmail.com>
2025-04-15 18:51:47 -0400

    more

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '', // Use relative paths for all assets and routing
+  base: process.env.VITE_BASE || '', // Use relative paths for all assets and routing
   plugins: [react()],
 })
