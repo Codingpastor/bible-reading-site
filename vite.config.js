d3a6e2d8a9c7851a289692fb8568b9baf9b57dff
Codingpastor <dawicks@gmail.com>
2025-04-15 21:41:42 -0400

    new system

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.VITE_BASE || '', // Use relative paths for all assets and routing
+  base: '/', // Set base path for GitHub Pages custom domain
   plugins: [react()],
 })
